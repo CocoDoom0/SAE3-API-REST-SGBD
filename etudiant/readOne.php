03691d9008434a11b1460fe306a0cbed250e9a47
Remi.mhx <remimahieux700@gmail.com>
2022-11-18 08:48:42 +0100

    - Etudiant API READONE crée Envoie en body raw pour rechercher
    {
        "idEtudiant": 1
    }

---
+++ etudiant/readOne.php
@@ -0,0 +1,66 @@
+<?php
+// Headers requis
+// Accès depuis n'importe quel site ou appareil (*)
+header("Access-Control-Allow-Origin: *");
+
+// Format des données envoyées
+header("Content-Type: application/json; charset=UTF-8");
+
+// Méthode autorisée
+header("Access-Control-Allow-Methods: GET");
+
+// Durée de vie de la requête
+header("Access-Control-Max-Age: 3600");
+
+// Entêtes autorisées
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+if($_SERVER['REQUEST_METHOD'] == 'GET'){
+    // La bonne méthode est utilisée
+    include_once '../config/database.php';
+    include_once '../models/etudiant.php';
+
+    // On instancie la base de données
+    $database = new database();
+    $db = $database->getConnexion();
+    // On instancie les etudiants
+    $etudiant = new etudiant($db);
+
+    $donnees = json_decode(file_get_contents("php://input"));
+    echo json_encode(array("message" => $donnees));
+    if(!empty($donnees->idEtudiant)){
+        $etudiant->idEtudiant = $donnees->idEtudiant;
+
+        // On récupère le produit
+        $etudiant->readOne();
+
+        // On vérifie si le produit existe
+        if($etudiant->nom != null){
+
+            $prod = [
+                "idEtudiant" => $etudiant->idEtudiant,
+                "nom" => $etudiant->nom,
+                "prenom" => $etudiant->prenom,
+                "parcours" => $etudiant->parcours,
+                "idDemiGroupe" => $etudiant->idDemiGroupe,
+            ];
+
+            // On envoie le code réponse 200 OK
+            http_response_code(200);
+
+            // On encode en json et on envoie
+            echo json_encode($prod);
+        }else{
+            // 404 Not found
+            http_response_code(404);
+
+            echo json_encode(array("message" => "Le produit n'existe pas."));
+        }
+
+    }
+}else{
+    // On gère l'erreur
+    http_response_code(405);
+    echo json_encode(["message" => "La méthode n'est pas autorisée"]);
+}
+?>
